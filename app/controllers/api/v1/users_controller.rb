143b2ae04759e03707c30cb87f1659d95162401a
sparkbold-git <sparkboldstudio@gmail.com>
2018-11-10 07:10:53 -0600

    fix validation

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,2 +1,18 @@
+# frozen_string_literal: true
+
 class Api::V1::UsersController < ApplicationController
+  def create
+    @user = User.create(user_params)
+    if @user.valid?
+      render json: { user: UserSerializer.new(@user) }, status: :created
+    else
+      render json: { error: 'failed to create user' }, status: :not_acceptable
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :password)
+  end
 end
