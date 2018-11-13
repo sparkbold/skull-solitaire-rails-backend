32a48a852c8e071a7a5aa08d6692ce7f4f283619
sparkbold-git <sparkboldstudio@gmail.com>
2018-11-13 11:28:07 -0600

    index games

--- app/controllers/api/v1/games_controller.rb
+++ app/controllers/api/v1/games_controller.rb
@@ -1,6 +1,11 @@
 class Api::V1::GamesController < ApplicationController
   skip_before_action :authorized
 
+  def index
+    @games = Game.all 
+    render json: @games 
+  end
+
 def create
     @game = Game.create(game_params)
     if @game.valid?
